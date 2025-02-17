5936fc1d35e424e2b7c0805f577c828afab79073
夏 雨轩 <2562833605@qq.com>
2025-02-17 16:48:21 +0800

    feat(learn-online-system): 添加系统管理模块
    - 新增 GlobalCorsConfig 跨域配置
    - 添加 Dictionary 数据字典相关功能
    - 实现 DictionaryController、DictionaryService 和 DictionaryMapper
    - 配置 MybatisPlus 和 LocalDateTime 序列化
    - 添加日志配置和 Swagger 文档

---
+++ learn-online-system/learn-online-system-service/src/main/java/com/xia/system/mapper/DictionaryMapper.java
@@ -0,0 +1,16 @@
+package com.xia.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xia.system.model.po.Dictionary;
+
+
+/**
+ * <p>
+ * 数据字典 Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface DictionaryMapper extends BaseMapper<Dictionary> {
+
+}

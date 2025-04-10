4f3f7c044fb08342bd78b298f967ea2d023f5b81
夏 雨轩 <2562833605@qq.com>
2025-04-11 00:38:28 +0800

    feat(media-service): 任务处理完成更新处理结果
    - 新增 saveProcessFinishStatus 方法，用于保存任务处理结果
    - 更新任务状态、错误信息等字段
    - 处理成功时，更新媒资文件 URL 并添加历史记录
    - 处理失败时，更新失败次数和错误信息

--- learn-online-media/learn-online-media-service/src/main/java/com/xia/media/service/MediaProcessService.java
+++ learn-online-media/learn-online-media-service/src/main/java/com/xia/media/service/MediaProcessService.java
@@ -22,4 +22,17 @@ public interface MediaProcessService {
      * @return
      */
     Boolean startTask(long taskId);
+
+
+    /**
+     * 任务处理完成需要更新任务处理结果，任务执行成功更新视频的URL、及任务处理结果，
+     * 将待处理任务记录删除，同时向历史任务表添加记录。
+     * @param taskId
+     * @param status
+     * @param fileId
+     * @param url
+     * @param errorMsg
+     */
+    void saveProcessFinishStatus(Long taskId, String status, String fileId, String url, String errorMsg);
+
 }
--- learn-online-media/learn-online-media-service/src/main/java/com/xia/media/service/impl/MediaProcessServiceImpl.java
+++ learn-online-media/learn-online-media-service/src/main/java/com/xia/media/service/impl/MediaProcessServiceImpl.java
@@ -1,19 +1,34 @@
 package com.xia.media.service.impl;
 
+import com.xia.media.mapper.MediaFilesMapper;
+import com.xia.media.mapper.MediaProcessHistoryMapper;
 import com.xia.media.mapper.MediaProcessMapper;
+import com.xia.media.model.po.MediaFiles;
 import com.xia.media.model.po.MediaProcess;
+import com.xia.media.model.po.MediaProcessHistory;
 import com.xia.media.service.MediaProcessService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @Service
 public class MediaProcessServiceImpl implements MediaProcessService {
 
 
     @Autowired
     private MediaProcessMapper mediaProcessMapper;
+
+    @Autowired
+    private MediaFilesMapper mediaFilesMapper;
+
+    @Autowired
+    private MediaProcessHistoryMapper mediaProcessHistoryMapper;
 
     /**
      * 根据分片参数获取待处理任务
@@ -36,5 +51,54 @@ public class MediaProcessServiceImpl implements MediaProcessService {
     public Boolean startTask(long taskId) {
         int i = mediaProcessMapper.updateTaskId(taskId);
         return i > 0;
+    }
+
+    /**
+     * 保存任务处理结果
+     * @param taskId
+     * @param status
+     * @param fileId
+     * @param url
+     * @param errorMsg
+     */
+    @Transactional
+    @Override
+    public void saveProcessFinishStatus(Long taskId, String status, String fileId, String url, String errorMsg) {
+        //根据任务id查询任务
+        MediaProcess mediaProcess = mediaProcessMapper.selectById(taskId);
+        if (mediaProcess == null) {
+            return;
+        }
+        //任务处理失败
+        if ("3".equals(status)) {
+            mediaProcess.setStatus("3");
+            mediaProcess.setFailCount(mediaProcess.getFailCount() + 1);
+            mediaProcess.setErrormsg(errorMsg);
+            mediaProcessMapper.updateById(mediaProcess);
+            log.debug("更新任务失败记录,{}", mediaProcess);
+            return;
+        }
+        //任务处理成功
+        if ("2".equals(status)) {
+            //更新任务记录
+            mediaProcess.setStatus("2");
+            mediaProcess.setUrl(url);
+            mediaProcess.setFinishDate(LocalDateTime.now());
+            mediaProcessMapper.updateById(mediaProcess);
+            //更新文件表记录
+            MediaFiles mediaFiles = mediaFilesMapper.selectById(fileId);
+            if(mediaFiles!=null){
+                //更新媒资文件中的访问url
+                mediaFiles.setUrl(url);
+                mediaFilesMapper.updateById(mediaFiles);
+            }
+            //添加到文件表历史记录表
+            MediaProcessHistory mediaProcessHistory = new MediaProcessHistory();
+            BeanUtils.copyProperties(mediaProcess, mediaProcessHistory);
+            mediaProcessHistoryMapper.insert(mediaProcessHistory);
+            //删除待处理任务记录
+            mediaProcessMapper.deleteById(mediaProcess.getId());
+        }
+
     }
 }

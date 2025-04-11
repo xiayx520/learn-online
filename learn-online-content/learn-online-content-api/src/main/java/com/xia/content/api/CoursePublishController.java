035032d120acc5eeaac294b9b84eab8a76dc578b
夏 雨轩 <2562833605@qq.com>
2025-04-11 14:43:31 +0800

    feat(content): 添加 Freemarker 模板测试
    - 新增 CoursePublishController 控制器，用于课程发布管理
    - 添加 test 方法测试 Freemarker 模板
    - 新增 Freemarker 配置和依赖- 创建 test.ftl 模板文件

---
+++ learn-online-content/learn-online-content-api/src/main/java/com/xia/content/api/CoursePublishController.java
@@ -0,0 +1,23 @@
+package com.xia.content.api;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@Slf4j
+@Api(tags = "课程发布管理")
+public class CoursePublishController {
+
+    @GetMapping("/testfreemarker")
+    public ModelAndView test(){
+        ModelAndView modelAndView = new ModelAndView();
+        //设置模型数据
+        modelAndView.addObject("name","小明");
+        //设置模板名称
+        modelAndView.setViewName("test");
+        return modelAndView;
+    }
+}

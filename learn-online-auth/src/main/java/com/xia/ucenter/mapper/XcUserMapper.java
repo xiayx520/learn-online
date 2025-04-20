a36cba642a9d402d576023d12251a0690162a88f
夏 雨轩 <2562833605@qq.com>
2025-04-20 14:49:58 +0800

    feat(auth): 创建认证服务- 新增 AuthApplication 启动类 - 添加用户认证请求参数 DTO - 创建基础配置文件和日志配置 - 实现简单登录控制器 - 配置 MyBatis-Plus - 添加多个实体类和对应的 Mapper 接口及 XML 文件 - 新增用户扩展信息 DTO - 更新项目父 POM 文件，添加认证模块子模块

---
+++ learn-online-auth/src/main/java/com/xia/ucenter/mapper/XcUserMapper.java
@@ -0,0 +1,15 @@
+package com.xia.ucenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xia.ucenter.model.po.XcUser;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcUserMapper extends BaseMapper<XcUser> {
+
+}
---
+++ learn-online-auth/src/main/java/com/xia/ucenter/mapper/XcUserRoleMapper.java
@@ -0,0 +1,15 @@
+package com.xia.ucenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xia.ucenter.model.po.XcUserRole;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcUserRoleMapper extends BaseMapper<XcUserRole> {
+
+}

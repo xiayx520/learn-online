4b6226d8b5a40b6ca3a74a6b7b4ce656a352e8cf
夏 雨轩 <2562833605@qq.com>
2025-04-21 19:06:16 +0800

    feat(auth): 实现认证授权功能
    - 新增 AuthorizationServer 配置类，实现 OAuth2 授权服务器配置
    - 添加 TokenConfig配置类，设置令牌存储和管理服务
    - 创建 WebSecurityConfig 配置类，定义用户认证和权限管理策略
    - 在 LoginController 中添加 PreAuthorize 注解，实现方法级别权限控制
    - 更新 pom.xml，添加 Spring Cloud Security 和 OAuth2 依赖

--- learn-online-auth/src/main/java/com/xia/auth/controller/LoginController.java
+++ learn-online-auth/src/main/java/com/xia/auth/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.xia.ucenter.mapper.XcUserMapper;
 import com.xia.ucenter.model.po.XcUser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,12 +38,14 @@ public class LoginController {
     }
 
     @RequestMapping("/r/r1")
-    public String r1() {
+    @PreAuthorize("hasAuthority('p1')")//拥有p1权限方可访问
+    public String r1(){
         return "访问r1资源";
     }
 
     @RequestMapping("/r/r2")
-    public String r2() {
+    @PreAuthorize("hasAuthority('p2')")//拥有p2权限方可访问
+    public String r2(){
         return "访问r2资源";
     }
 

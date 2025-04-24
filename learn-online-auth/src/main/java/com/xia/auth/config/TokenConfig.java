3c6d0d724608c1c3174cc4e0441c6937c1fd70a7
夏 雨轩 <2562833605@qq.com>
2025-04-24 15:43:27 +0800

    feat(gateway): 添加网关认证过滤器和JWT支持
    - 新增 GatewayAuthFilter 实现全局过滤器，用于认证和授权
    - 添加 TokenConfig 配置类，支持 JWT 令牌存储和转换- 创建 RestErrorResponse 类用于封装错误响应
    - 新增 SecurityConfig配置类，启用 WebFlux 安全过滤
    - 更新 learn-online-auth 中的 TokenConfig，支持 JWT 令牌增强

--- learn-online-auth/src/main/java/com/xia/auth/config/TokenConfig.java
+++ learn-online-auth/src/main/java/com/xia/auth/config/TokenConfig.java
@@ -5,8 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
 
 /**
  * @author Administrator
@@ -15,14 +19,32 @@ import org.springframework.security.oauth2.provider.token.store.InMemoryTokenSto
 @Configuration
 public class TokenConfig {
 
+    String SIGNING_KEY = "mq123";
+
     @Autowired
     TokenStore tokenStore;
 
+    @Autowired
+    private JwtAccessTokenConverter accessTokenConverter;
+
+//    @Bean
+//    public TokenStore tokenStore() {
+//        //使用内存存储令牌（普通令牌）
+//        return new InMemoryTokenStore();
+//    }
+
     @Bean
     public TokenStore tokenStore() {
-        //使用内存存储令牌（普通令牌）
-        return new InMemoryTokenStore();
+        return new JwtTokenStore(accessTokenConverter());
     }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(SIGNING_KEY);
+        return converter;
+    }
+
 
     //令牌管理服务
     @Bean(name="authorizationServerTokenServicesCustom")
@@ -30,6 +52,11 @@ public class TokenConfig {
         DefaultTokenServices service=new DefaultTokenServices();
         service.setSupportRefreshToken(true);//支持刷新令牌
         service.setTokenStore(tokenStore);//令牌存储策略
+
+        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+        tokenEnhancerChain.setTokenEnhancers(Arrays.asList(accessTokenConverter));
+        service.setTokenEnhancer(tokenEnhancerChain);
+
         service.setAccessTokenValiditySeconds(7200); // 令牌默认有效期2小时
         service.setRefreshTokenValiditySeconds(259200); // 刷新令牌默认有效期3天
         return service;

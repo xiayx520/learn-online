5abae52627ed59da65fd6a64f89248c9614e4bad
夏 雨轩 <2562833605@qq.com>
2025-04-25 22:29:00 +0800

    feat(auth): 添加验证码验证功能- 在 AuthApplication 中启用 Feign 客户端 - 新增 CheckCodeClient 接口，用于调用验证码服务 - 实现 CheckCodeClientFactory，提供熔断处理 - 在 PasswordAuthServiceImpl 中集成验证码验证逻辑 - 更新 bootstrap.yml，添加 feign 配置

--- learn-online-auth/src/main/java/com/xia/ucenter/service/impl/PasswordAuthServiceImpl.java
+++ learn-online-auth/src/main/java/com/xia/ucenter/service/impl/PasswordAuthServiceImpl.java
@@ -1,11 +1,13 @@
 package com.xia.ucenter.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.xia.auth.feignclient.CheckCodeClient;
 import com.xia.ucenter.mapper.XcUserMapper;
 import com.xia.ucenter.model.dto.AuthParamsDto;
 import com.xia.ucenter.model.dto.XcUserExt;
 import com.xia.ucenter.model.po.XcUser;
 import com.xia.ucenter.service.AuthService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,8 +22,27 @@ public class PasswordAuthServiceImpl implements AuthService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private CheckCodeClient checkCodeClient;
+
     @Override
     public XcUserExt execute(AuthParamsDto authParamsDto) {
+
+        //校验验证码
+        String checkcode = authParamsDto.getCheckcode();
+        String checkcodekey = authParamsDto.getCheckcodekey();
+
+        if(StringUtils.isBlank(checkcodekey) || StringUtils.isBlank(checkcode)){
+            throw new RuntimeException("验证码为空");
+
+        }
+        Boolean verify = checkCodeClient.verify(checkcodekey, checkcode);
+        if(!verify){
+            throw new RuntimeException("验证码输入错误");
+        }
+
+
+
         //账号
         String username = authParamsDto.getUsername();
         XcUser user = xcUserMapper.selectOne(new LambdaQueryWrapper<XcUser>().eq(XcUser::getUsername, username));

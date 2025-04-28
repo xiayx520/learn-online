1f71b69e0778d1b115671c9716faaf6252e0c436
夏 雨轩 <2562833605@qq.com>
2025-04-28 18:36:35 +0800

    feat(learn-online): 添加订单管理模块

---
+++ learn-online-orders/learn-online-orders-service/src/main/java/com/xia/orders/mapper/XcOrdersMapper.java
@@ -0,0 +1,15 @@
+package com.xia.orders.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xia.orders.model.po.XcOrders;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcOrdersMapper extends BaseMapper<XcOrders> {
+
+}
---
+++ learn-online-orders/learn-online-orders-service/src/main/java/com/xia/orders/mapper/XcPayRecordMapper.java
@@ -0,0 +1,15 @@
+package com.xia.orders.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xia.orders.model.po.XcPayRecord;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcPayRecordMapper extends BaseMapper<XcPayRecord> {
+
+}
